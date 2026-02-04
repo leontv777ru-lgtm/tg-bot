bba9aa3f2e7a0a954cb4739b8ae07896789a2e50
leontv777ru-lgtm <leontv777.ru@mail.ru>
2026-02-04 13:51:43 +0500

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -1,62 +1,23 @@
-from aiogram import Router, F, types
-from aiogram.fsm.context import FSMContext
-
-from config import VERIF_CHANNEL_ID
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database.db import database
-from keyboards.client import ClientKeyboard
-from other.languages import languages
-from other.states import ChangeReferral
 
-router = Router()
+class ClientKeyboard:
 
-@router.message(F.chat.func(lambda chat: chat.id == int(VERIF_CHANNEL_ID)))
-async def channel_verification_handler(message: types.Message):
-    try:
-        user_id = int(message.text)
-    except ValueError:
-        return
+    @staticmethod
+    async def back_keyboard(lang: str):
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text="⬅ Назад", callback_data="back")]
+            ]
+        )
 
-    user = await database.get_user_info(user_id)
-    if user is None:
-        return
+    @staticmethod
+    async def get_signal_keyboard(lang: str):
+        ref = await database.get_ref()
 
-    lang = await database.get_lang(user_id)
-    await database.update_verified(user_id)
-
-    await message.bot.send_message(
-        chat_id=user_id,
-        text=languages[lang]["success_registration"],
-        reply_markup=await ClientKeyboard.get_signal_keyboard(lang),
-        parse_mode="HTML"
-    )
-
-@router.callback_query(F.data == "change_ref")
-async def change_referral_callback_handler(
-    callback: types.CallbackQuery,
-    state: FSMContext
-):
-    lang = await database.get_lang(callback.from_user.id)
-
-    await callback.message.delete()
-    await callback.message.answer(
-        languages[lang]["enter_new_ref"],
-        parse_mode="HTML"
-    )
-
-    await state.set_state(ChangeReferral.input_ref)
-
-@router.message(ChangeReferral.input_ref)
-async def change_referral_message_state(
-    message: types.Message,
-    state: FSMContext
-):
-    lang = await database.get_lang(message.from_user.id)
-
-    await database.edit_ref(message.text)
-
-    await message.answer(
-        languages[lang]["ref_changed"],
-        parse_mode="HTML"
-    )
-
-    await state.clear()
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text="🚀 Зарегистрироваться", url=ref)],
+                [InlineKeyboardButton(text="🔄 Сменить реферальную ссылку", callback_data="change_ref")]
+            ]
+        )
