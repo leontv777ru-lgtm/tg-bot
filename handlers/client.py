8c3e5c59a68770bd6c5a87d293d53da2c20cb867
leontv777ru-lgtm <leontv777.ru@mail.ru>
2026-02-04 15:40:26 +0500

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -1,57 +1,17 @@
-from aiogram import Router, F, types
-from aiogram.fsm.context import FSMContext
-
-from config import VERIF_CHANNEL_ID
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database.db import database
-from keyboards.client import ClientKeyboard
-from other.languages import languages
-from aiogram.fsm.state import StatesGroup, State
 
-router = Router()
+class ClientKeyboard:
 
-class ChangeReferral(StatesGroup):
-    input_ref = State()
+    @staticmethod
+    async def get_signal_keyboard(lang: str):
+        ref = await database.get_ref()
+        return InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="🚀 Регистрация", url=ref)]
+        ])
 
-@router.message(F.chat.func(lambda chat: chat.id == int(VERIF_CHANNEL_ID)))
-async def channel_verification_handler(message: types.Message):
-    if await database.get_user(message.text) is None:
-        lang = await database.get_lang(int(message.text))
-        await database.update_verifed(message.text)
-
-        await message.bot.send_message(
-            chat_id=int(message.text),
-            text=languages[lang]["success_registration"],
-            reply_markup=await ClientKeyboard.get_signal_keyboard(lang),
-            parse_mode="HTML"
-        )
-
-@router.callback_query(F.data == "change_ref")
-async def change_referral_callback_handler(
-    callback: types.CallbackQuery,
-    state: FSMContext
-):
-    lang = await database.get_lang(callback.from_user.id)
-
-    await callback.message.delete()
-    await callback.message.answer(
-        languages[lang]["enter_new_ref"],
-        parse_mode="HTML"
-    )
-
-    await state.set_state(ChangeReferral.input_ref)
-
-@router.message(ChangeReferral.input_ref)
-async def change_referral_message_state(
-    message: types.Message,
-    state: FSMContext
-):
-    lang = await database.get_lang(message.from_user.id)
-
-    await database.edit_ref(message.text)
-
-    await message.answer(
-        languages[lang]["ref_changed"],
-        parse_mode="HTML"
-    )
-
-    await state.clear()
+    @staticmethod
+    async def back_keyboard(lang: str):
+        return InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="⬅ Назад", callback_data="back")]
+        ])
