e911f0c7575e4d4f9248c848860ce2fe6d52de02
leontv777ru-lgtm <leontv777.ru@mail.ru>
2026-02-06 15:22:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ from handlers.client import router as client_router
 from handlers.admin import router as admin_router
 from database.db import DataBase
 
-
 logger = logging.getLogger(__name__)
-
 
 async def main():
     logging.basicConfig(
         level=logging.INFO,
-        format=f'[BOT] '
-               f'{u"%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s"}')
+        format='[BOT] %(filename)s:%(lineno)d %(levelname)-8s [%(asctime)s] - %(name)s - %(message)s'
+    )
+
     logger.info("Starting bot...")
 
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-    bot = Bot(BOT_TOKEN)
+
     dp.include_routers(client_router, admin_router)
 
-    dp.startup.register(DataBase.on_startup)
-    await bot.delete_webhook(drop_pending_updates=True)
+    # ✅ ВАЖНО: оборачиваем в lambda
+    dp.startup.register(lambda: DataBase.on_startup())
 
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
